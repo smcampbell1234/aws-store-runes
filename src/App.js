a6003bca6c8cf2c1b7032330bf4b311af2737739
Sean Campbell <writer.sandiego@gmail.com>
2022-08-24 01:18:31 -0700

    base pages & navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './style/App.css';
+import Navbar from './components/Navbar';
+import {
+  Error,
+  Home,
+  Login,
+  Products,
+  ProductDetails
+} from './pages'
 
 function App() {
   return (
     <div className="App">
       <h2>App Page</h2>
+      <Navbar />
+      <Home />
+      <Login />
+      <Products />
+      <ProductDetails />
+      <Error />
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Error from './Error';
+import Home from './Home';
+import Login from './Login';
+import Products from './Products';
+import ProductDetails from './ProductDetails';
+
+export {
+  Error,
+  Home,
+  Login,
+  Products,
+  ProductDetails
+}
