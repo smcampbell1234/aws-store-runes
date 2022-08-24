6de536ada59263c4797bd6319d20cf7c068fba60
Sean Campbell <writer.sandiego@gmail.com>
2022-08-24 02:01:09 -0700

    Routing and Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './style/App.css';
 import Navbar from './components/Navbar';
-import {
-  Error,
-  Home,
-  Login,
-  Products,
-  ProductDetails
-} from './pages'
+import { Error, Home, Login, Products,ProductDetails} from './pages'
 
 function App() {
   return (
     <div className="App">
-      <h2>App Page</h2>
-      <Navbar />
-      <Home />
-      <Login />
-      <Products />
-      <ProductDetails />
-      <Error />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="store" element={<Products />} />
+          <Route path="products/:prodId" element={<ProductDetails />} />
+          <Route path="error" element={<Error />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
+import { NavLink, useNavigate } from "react-router-dom";
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <nav className="navbar-wrapper">
+      <NavLink 
+        to="/"
+        className={({isActive}) => (isActive ? "link active" : "link")}
+        >Home</NavLink>
+      <NavLink 
+        to="/store"
+        className={({isActive}) => (isActive ? "link active" : "link")}
+        >Store</NavLink>
+      <NavLink 
+        to="/login"
+        className={({isActive}) => (isActive ? "link active" : "link")}
+        >Login</NavLink>
+    </nav>
   )
 }
 
