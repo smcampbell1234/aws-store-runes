1833735abbcbd9a727eb541a9ab8dd20d41a9174
Sean Campbell <writer.sandiego@gmail.com>
2022-08-26 02:38:05 -0700

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Error() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/")
+    },2800)
+  })
   return (
-    <div>
-      <h2>Error</h2>
+    <div className="error-wrapper">
+      <img src="https://img.freepik.com/premium-vector/bearded-viking-warrior-head_9645-1418.jpg?w=2000" alt="viking" />
+      <h2>Error: 405 - Arrrr! You shouldn't be here.</h2>
+      <h3 onClick={() => navigate(-1)}>Back</h3>
     </div>
   )
 }
