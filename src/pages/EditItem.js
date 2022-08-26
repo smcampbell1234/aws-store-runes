e2e8f146b50321c1cccda98f39ddbfa27ffb2d58
Sean Campbell <writer.sandiego@gmail.com>
2022-08-25 21:31:05 -0700

    edit item & get API call

--- src/pages/EditItem.js
+++ src/pages/EditItem.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 let url = "https://iu02eobxgk.execute-api.us-east-1.amazonaws.com/dev/products";
+
 function EditItem() {
   const [items,setItems] = useState([])
 
@@ -19,6 +20,24 @@ function EditItem() {
     fetchProducts()
   },[])
 
+  const deleteItem = async (id) => {
+    try {
+      const urlDelete = url + `/${id}`
+      const response = await fetch(urlDelete,{
+        method: 'DELETE'
+      })
+      const data = await response.json();
+      console.log("Success : ",data)
+
+      // remove from list
+      let newList = items.filter((item) => item.prodId !== id)
+      setItems(newList);
+
+    } catch (err) {
+      console.log(`Failed to delete item with id ${id}`)
+    }
+  }
+
   console.log(".... Items : ",items)
   return (
     <div>
@@ -33,15 +52,15 @@ function EditItem() {
               <div className="item-picture"><img src={item.image} alt={item.image} /></div>
               <div className="item-info">
                 <div className="item-title">{item.title}</div>
-                <div className="item-category">category: {item.category}</div>
+                <div className="item-category">Category: {item.category}</div>
                 <div className="item-id">id: {item.prodId}</div>
               </div>
               <div className="item-price-data">
                 <div className="item-price">Price: ${item.price}</div>
                 <div className="sitem-ale">Sale: {item.sale}%</div>
               </div>
-              <div className="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum convallis iaculis. Etiam arcu nibh, sagittis sit amet scelerisque non, scelerisque auctor felis. Nullam non quam facilisis, dapibus velit et, scelerisque arcu. Nunc at justo libero.  </div>
-              <div className="item-delete">delete</div>
+              <div className="item-description">{item.description}</div>
+              <div className="item-delete" onClick={() => deleteItem(item.prodId)}>delete</div>
             </div>
           )
         })
